2f506719c25c90b7b162674309f0bb3e5e856b76
atti199 <atti199@gmail.com>
2023-07-28 21:17:38 +0200

    terra

---
+++ script.js
@@ -0,0 +1,28 @@
+var sheetId = "1khPsuozntGp8KXOpLYocFxQROE_qGbMk";  // Replace with your Sheet ID
+var apiKey = "AIzaSyBFEHQpGzSbF36WE8FySkFARJstBczTEiQ";  // Replace with your API Key
+var range = "Sheet1!A1:C10";  // The range of cells you want to access
+
+var url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`;
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    var labels = data.values.map(row => row[0]);  // Assuming first column contains labels
+    var values = data.values.map(row => row[1]);  // Assuming second column contains values
+
+    var ctx = document.getElementById('chartContainer').getContext('2d');
+    new Chart(ctx, {
+      type: 'line',  // Change this to the type of chart you want
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'My Dataset',
+          data: values,
+          backgroundColor: 'rgba(0, 123, 255, 0.5)'
+        }]
+      },
+      options: {
+        responsive: true
+      }
+    });
+  });
